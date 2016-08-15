12e529fd92884843432033c69b663d04f45778fc
Emre Hasegeli <emre@hasegeli.com>
2016-08-15 15:25:52 +0200

    [FEATURE] Accept multiple queries on postgres_query.py

--- postgres_query.py
+++ postgres_query.py
@@ -22,27 +22,33 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--prefix', default='postgres_query')
     parser.add_argument('--dbname', default='postgres')
-    parser.add_argument('--query', required=True)
+    parser.add_argument(
+        '--query',
+        required=True,
+        action='append',
+        dest='queries',
+    )
 
     return vars(parser.parse_args())
 
 
-def main(prefix, dbname, query):
+def main(prefix, dbname, queries):
     """The main program"""
     now = str(int(time.time()))
 
-    for line in connect_and_execute(query, database=dbname):
-        for key, value in line.items():
-            print(prefix + '.' + key, value, now)
+    with psycopg2.connect(database=dbname) as conn:
+        for query in queries:
+            for line in execute(conn, query):
+                for key, value in line.items():
+                    print(prefix + '.' + key, value, now)
 
 
-def connect_and_execute(query, database='postgres'):
-    """Connect to database, execute given query and return fetched results"""
+def execute(conn, query):
+    """Execute given query and return fetched results"""
+    with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+        cursor.execute(query)
 
-    with psycopg2.connect(database=database) as conn:
-        with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            cursor.execute(query)
-            return cursor.fetchall()
+        return cursor.fetchall()
 
 
 if __name__ == '__main__':
