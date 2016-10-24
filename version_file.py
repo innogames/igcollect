e228bad3dd69105f9ef33a804cd1c58e8f470df7
Emre Hasegeli <emre@hasegeli.com>
2016-10-24 17:19:10 +0200

    Code review of version_file.py

---
+++ version_file.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#
+# igcollect - Sent currently deployed game version.
+#
+# Copyright (c) 2016 InnoGames GmbH
+#
+
+from argparse import ArgumentParser
+from os.path import exists
+from time import time
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--prefix', default='version')
+    parser.add_argument('--filename', default='version')
+
+    return vars(parser.parse_args())
+
+
+def main(prefix, filename):
+    if not exists(filename):
+        raise Exception('No file on "{0}"'.format(filename))
+
+    with open(filename) as fh:
+        line = fh.readline()
+
+    revision = line.strip().rsplit(' ', 1)[-1]
+    if revision.startswith('v'):
+        revision = revision[1:]
+    revision = revision.replace('.', '_')
+    print('{}.{} 1 {}'.format(prefix, revision, int(time())))
+
+
+if __name__ == '__main__':
+    main(**parse_args())
