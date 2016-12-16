95924cc920f22fc100cc43a2f345be1b9981d4af
Emre Hasegeli <emre@hasegeli.com>
2016-12-16 11:38:51 +0100

    Review mysql_replication_delay.py

--- src/mysql_replication_delay.py
+++ src/mysql_replication_delay.py
@@ -1,33 +1,48 @@
 #!/usr/bin/env python
 #
-# igcollect - Mysql replication delay
+# igcollect - MySQL replication delay
 #
 # Copyright (c) 2016, InnoGames GmbH
 #
 
-import time
-import socket
-import subprocess
-import argparse
+from argparse import ArgumentParser
+from socket import gethostname
+from subprocess import check_output
+from time import time
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--database', dest='db', type=str, required=True, help='Database to read timestamps from')
-parser.add_argument('-m', '--master-id', dest='id', type=int, required=True, help='server_id of the writer of timestamps')
-args = parser.parse_args()
 
-hostname = socket.gethostname().replace(".", "_")
-now =  str(int(time.time()))
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--database',
+        type=str,
+        required=True,
+        help='Database to read timestamps from',
+    )
+    parser.add_argument(
+        '--master-id',
+        type=int,
+        required=True,
+        help='server_id of the writer of timestamps',
+    )
+    return parser.parse_args()
 
-delay = subprocess.check_output(
-    [
+
+def main():
+    args = parse_args()
+    hostname = gethostname().replace('.', '_')
+    now = int(time())
+    template = 'servers.{}.software.mysql.status.seconds_behind_master {} {}'
+    delay = check_output((
         '/usr/bin/pt-heartbeat',
         '--check',
-        '--database={0}'.format(args.db),
-        '--master-server-id={0}'.format(args.id)
-    ]).strip()
+        '--database={0}'.format(args.database),
+        '--master-server-id={0}'.format(args.master_id)
+    )).strip()
 
-try:
-    float(delay)
-    print('servers.{0}.software.mysql.status.seconds_behind_master {1} {2}'.format(hostname, delay, now))
-except ValueError:
-    pass
+    # Make sure the command returned a proper value by casting it to float
+    print(template.format(hostname, float(delay), now))
+
+
+if __name__ == '__main__':
+    main()
