1a28e155c8551b5cd1d754b8ec289893f1b6ff2f
Emre Hasegeli <emre@hasegeli.com>
2017-05-03 12:02:07 +0200

    Generalise linux_numa_memory.py

--- src/linux_numa.py
+++ src/linux_numa.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 #
-# igcollect - Linux NUMA memory
+# igcollect - Linux NUMA metrics
 #
-# Copyright (c) 2016, InnoGames GmbH
+# Copyright (c) 2017, InnoGames GmbH
 #
 
 from argparse import ArgumentParser
@@ -11,22 +11,22 @@ from time import time
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument('--prefix', default='numa_memory')
+    parser.add_argument('--prefix', default='numa')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    path = '/sys/devices/system/node/node{}/meminfo'
-    template = args.prefix + '.node{}.{} {} ' + str(int(time()))
+    path = '/sys/devices/system/node/node{}/{}'
+    template = args.prefix + '.node{}.{}.{} {} ' + str(int(time()))
 
     for node in get_numa_nodes():
-        with open(path.format(node)) as fd:
+        with open(path.format(node, 'meminfo')) as fd:
             for line in fd:
                 line_split = line.strip().split()
                 key = line_split[2].rstrip(':')
                 value = int(line_split[3])
-                print(template.format(node, key, value))
+                print(template.format(node, 'memory', key, value))
 
 
 def parse_split_file(filename):
