298ecee9012f73711904adac34f5443d222c7c47
Emre Hasegeli <emre@hasegeli.com>
2017-10-30 14:04:27 +0100

    mysql_query: Unify coding style

--- src/mysql_query.py
+++ src/mysql_query.py
@@ -10,13 +10,14 @@
 #
 from __future__ import print_function
 
-import mysql.connector
-
 from argparse import ArgumentParser
 from time import time
 
+from mysql.connector import connect
+from mysql.connector.cursor import MySQLCursor
 
-class MySQLCursorDict(mysql.connector.cursor.MySQLCursor):
+
+class MySQLCursorDict(MySQLCursor):
     def _row_to_python(self, rowdata, desc=None):
         row = super(MySQLCursorDict, self)._row_to_python(rowdata, desc)
         if row:
@@ -35,7 +36,8 @@ def parse_args():
         '--query',
         required=True,
         action='append',
-        dest='queries', )
+        dest='queries',
+    )
     parser.add_argument('--key-column')
     return parser.parse_args()
 
@@ -46,21 +48,22 @@ def main():
     items = []
     now = str(int(time()))
 
-    cnx = mysql.connector.connect(
+    cnx = connect(
         user=args.user,
         password=args.password,
         host=args.host,
-        db=args.dbname, 
-        )
+        db=args.dbname,
+    )
     cur = cnx.cursor(cursor_class=MySQLCursorDict)
     for query in args.queries:
         cur.execute(query)
         if not cur.rowcount:
             raise Exception('No result')
         rows = cur.fetchall()
-        items.extend(
-            get_row_data(rows, args.key_column)
-            if args.key_column else get_column_data(rows))
+        if args.key_column:
+            items.extend(get_row_data(rows, args.key_column))
+        else:
+            items.extend(get_column_data(rows))
 
     for key, value in items:
         print(args.prefix + '.' + key, value, now)
