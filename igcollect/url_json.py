22f8b24b112254ca5a5aa4a26c7348b7ff57e7ce
Daniel Kröger <daniel.kroeger@innogames.com>
2020-01-06 14:35:11 +0100

    feat(url_json): Add (optional) JSON path support
    Add --json-path parameter which takes one or more JSON path expressions and
    tries to parse them from given URL result JSON.
    
    * Add --json-path parameter to allow passing JSON path expression
    * Add description to help
    * Add example usage to help

--- igcollect/url_json.py
+++ igcollect/url_json.py
@@ -12,13 +12,31 @@ import json
 
 
 def parse_args():
-    parser = ArgumentParser()
+    parser = ArgumentParser(usage=(
+        'python3 igcollect/url_json.py'
+        ' --prefix servers.HOSTNAME.software.uwsgi'
+        ' --url http://localhost:8080'
+        ' --key pid'
+        ' --json-path workers.[*].requests workers.[*].respawn_count'
+    ), description=(
+        'Query JSON from URL and extract metrics for graphite text protocol'
+    ))
     parser.add_argument('--prefix', default='url_json')
     parser.add_argument('--url', default='http://localhost/')
     parser.add_argument(
         '--key',
         nargs='+',
-        dest='keys', )
+        dest='keys',
+        help='Plain string to match JSON key e.g.: workers.requests',
+    )
+    parser.add_argument(
+        '--json-path',
+        nargs='+',
+        dest='jsonpaths',
+        help=(
+            'JSON path to match key e.g.: workers[*].requests '
+            '(requires: jsonpath-rw module)')
+    )
     return parser.parse_args()
 
 
@@ -29,9 +47,20 @@ def main():
 
     template = args.prefix + '.{} {} ' + str(int(time()))
 
-    for key, value in data.items():
-        if key in args.keys or not args.keys:
-            print(template.format(key, value))
+    if args.keys:
+        for key, value in data.items():
+            if key in args.keys:
+                print(template.format(key, value))
+
+    if args.jsonpaths:
+        from jsonpath_rw import parse
+
+        for path in args.jsonpaths:
+            for match in parse(path).find(data):
+                # Graphite text protocol can not process square brackets
+                graphite_path = str(match.full_path).replace('[', '').replace(
+                    ']', '')
+                print(template.format(graphite_path, match.value))
 
 
 if __name__ == '__main__':
