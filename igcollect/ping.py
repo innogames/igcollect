fb3181ac777b2bfaca2a2244624262508da04c4a
Christian Urich <Jowe1999@JoweBook.local>
2020-03-05 10:29:17 +0100

    fix(threading) enhanced readability

--- igcollect/ping.py
+++ igcollect/ping.py
@@ -4,10 +4,10 @@
 Copyright (c) 2020 InnoGames GmbH
 """
 
-# this script is used to collect ping data to evaluate availability and latency
-# of our network (this should replace smokeping in the longrun)
+# This script is used to collect ping data to evaluate availability and latency
+# of our network and make it easy to create a Graphite Dashboard
 #
-# Please Not that the package fping has to be installed on your Linux server!
+# Please note that the package fping has to be installed on your Linux server!
 
 
 import argparse
@@ -36,21 +36,26 @@ def parse_args():
 def main():
     args = parse_args()
 
+    chunked_hosts = [args.hosts[i:i + 10]
+                     for i in range(0, len(args.hosts), 10)]
+
+    data = [(args.prefix, chunk, args.count, args.timeout)
+            for chunk in chunked_hosts]
+
     with Pool(cpu_count() * 2) as p:
-        for i in range(len(args.hosts) % 10):
-            p.starmap(ping, [(args.prefix, args.hosts[i*10:i*10+9],
-                              args.count, args.timeout)])
-        p.starmap(ping, [(args.prefix,
-                          args.hosts[(len(args.hosts) % 10) * 10:
-                                     len(args.hosts) - 1],
-                          args.count, args.timeout)])
+        p.starmap(ping, data)
+
 
 
 def ping(prefix, hosts, count, timeout):
     hosts_string = " ".join(hosts)
     # -p calculates the time fping waits between single ping probes
-    cmd = ('fping -B1 -q -C {} -p {} -t {} {}'.format(count, (20/count) * 1800,
-                                                      timeout, hosts_string))
+    # fping used 20 probes/min by default und set this value to 1800
+    # if you use 30 probes the -p time should goe down, for 10 up
+    # 0 would not make any sense to enter in to this script
+    cmd = 'fping -B1 -q -C {} -p {} -t {} {}'
+    cmd = cmd.format(count, (20/count) * 1800, timeout, hosts_string)
+
     output = subprocess.getoutput(cmd).split('\n')
     for line in output:
         data = {}
@@ -60,7 +65,6 @@ def ping(prefix, hosts, count, timeout):
         # first value would be empty
         pings.pop(0)
 
-        print(pings)
         avg = 0
         for i, ping in enumerate(pings):
             if pings[i] == '-':
