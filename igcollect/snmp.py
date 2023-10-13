4718666c2915d62ec4430da9cea4c91808e15235
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2023-10-13 18:36:45 +0200

    Move SNMP functions to a shared library

---
+++ igcollect/snmp.py
@@ -0,0 +1,110 @@
+"""igcollect - SNMP common library
+"""
+
+
+from pysnmp import proto, error
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+from pysnmp.entity.rfc3413.oneliner.cmdgen import (
+    CommunityData,
+    UsmUserData,
+    usmHMACSHAAuthProtocol,
+    usmAesCfb128Protocol,
+    usmDESPrivProtocol,
+)
+import re
+import sys
+
+class IgCollectSNMPException(Exception):
+    pass
+
+# Predefine some variables, it makes this program run a bit faster.
+cmd_gen = cmdgen.CommandGenerator()
+
+def get_snmp_connection(args):
+    """ Prepare SNMP transport agent.
+
+        Connection over SNMP v2c and v3 is supported.
+        The choice of authentication and privacy algorithms for v3 is
+        arbitrary, matching what our switches can do.
+    """
+
+    if args.community:
+        auth_data = CommunityData(args.community, mpModel=1)
+    else:
+        if args.priv_proto == 'des':
+            priv_proto = usmDESPrivProtocol
+        if args.priv_proto == 'aes':
+            priv_proto = usmAesCfb128Protocol
+
+        auth_data = UsmUserData(
+            args.user, args.auth, args.priv,
+            authProtocol=usmHMACSHAAuthProtocol,
+            privProtocol=priv_proto,
+        )
+
+    transport_target = cmdgen.UdpTransportTarget((args.host, 161))
+
+    return {
+        'auth_data': auth_data,
+        'transport_target': transport_target,
+    }
+
+
+def get_snmp_value(snmp, OID):
+    """ Get a single value from SNMP """
+
+    errorIndication, errorStatus, errorIndex, varBinds = cmd_gen.getCmd(
+        snmp['auth_data'],
+        snmp['transport_target'],
+        OID,
+    )
+    if errorIndication:
+        raise IgCollectSNMPException('Unable to get SNMP value: {}'.
+                              format(errorIndication))
+
+    return convert_snmp_type(varBinds)
+
+
+def get_snmp_table(snmp, OID):
+    """ Fetch a table from SNMP.
+
+        Returned is a dictionary mapping the last number of OID (converted to
+        Python integer) to value (converted to int or str).
+    """
+    ret = {}
+    errorIndication, errorStatus, errorIndex, varBindTable = cmd_gen.bulkCmd(
+        snmp['auth_data'],
+        snmp['transport_target'],
+        0,  # nonRepeaters
+        25,
+        OID,
+    )
+    for varBind in varBindTable:
+        # Oh the joy of pysnmp library!
+        # When the nonrepeaters value above is 0, we might get objects from
+        # another snmp tree on some hardware, for example from cisco routers.
+        # we can set it to 1 but then we have high cpu usage. So keep it 0
+        # and manually check if we are still in the same tree.
+        # OIDs we query for must not start with a dot.
+        if not str(varBind[0][0]).startswith(OID):
+            break
+        if errorIndication:
+            raise IgCollectSNMPException('Unable to get SNMP value: {}'.
+                                  format(errorIndication))
+        index = int(str(varBind[0][0][-1:]))
+        ret[index] = convert_snmp_type(varBind)
+
+    return ret
+
+
+def convert_snmp_type(varBinds):
+    """ Convert SNMP data types to something more convenient: int or str """
+
+    val = varBinds[0][1]
+    if type(val) in [
+        proto.rfc1902.Integer,
+        proto.rfc1902.Counter32,
+        proto.rfc1902.Counter64,
+    ]:
+        return int(val)
+    return str(val)
