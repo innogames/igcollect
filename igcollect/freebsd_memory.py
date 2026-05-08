b50ddda8de5176171d980316997f077a8a4441b5
Kajetan Staszkiewicz <kajetan.staszkiewicz@innogames.com>
2026-05-08 12:57:16 +0200

    Update memory types for FreeBSD 15

--- igcollect/freebsd_memory.py
+++ igcollect/freebsd_memory.py
@@ -10,6 +10,19 @@ from time import time
 import sysctl
 
 
+# Translate sysctl to human-readable names in Grafana
+MEMORY_TYPES = {
+    'v_page_count': 'total',
+    'v_free_count': 'free',
+    'v_wire_count': 'wired',
+    'v_user_wire_count': 'user_wired',
+    'v_nofree_count': 'nofree',
+    'v_active_count': 'active',
+    'v_inactive_count': 'inactive',
+    'v_laundry_count': 'laundry',
+}
+
+
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('--prefix', default='memory')
@@ -19,22 +32,24 @@ def parse_args():
 def parse_memory_info():
     memory_info={}
 
-    memory_info['physmem'] = sysctl.filter('hw.physmem')[0].value
-    pagesize = sysctl.filter('hw.pagesize')[0].value
+    pagesize = sysctl.filter('vm.stats.vm.v_page_size')[0].value
 
     memory_data = sysctl.filter('vm.stats.vm')
     for line in memory_data:
         name = line.name.split('.')[-1]
         # After multiplying by page size they are not _count anymore
+        memory_type = MEMORY_TYPES.get(name)
+        if not memory_type:
+            continue
         if name.endswith('_count'):
-            name = name.replace('_count', '')
             if type(line.value) == bytearray:
                 # py-sysctl lack support for CTLTYPE_U32
                 # https://lists.freebsd.org/pipermail/freebsd-current/2018-July/070344.html
                 value = int.from_bytes(line.value, byteorder='little', signed=False)
             else:
                 value = line.value
-            memory_info[name] = value * pagesize
+
+            memory_info[memory_type] = value * pagesize
 
     return memory_info
 
@@ -42,17 +57,9 @@ def parse_memory_info():
 def main():
     args = parse_args()
 
-    mi = parse_memory_info()
-    mem_gap_sys = mi['physmem'] - mi['v_page']
-
     template = args.prefix + '.{} {} ' + str(int(time()))
-    print(template.format('total', mi['physmem']))
-    print(template.format('active', mi['v_active']))
-    print(template.format('inactive', mi['v_inactive']))
-    print(template.format('wired', mi['v_wire']))
-    print(template.format('cache', mi['v_cache']))
-    print(template.format('free', mi['v_free']))
-    print(template.format('gap_sys', mem_gap_sys))
+    for memory_name, memory_value in parse_memory_info().items():
+        print(template.format(memory_name, memory_value))
 
 if __name__ == '__main__':
     main()
